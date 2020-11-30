a58a0be78d730bda51af7faca03f7f6363ef2027
tino.haerta <tino.haerta@dmtm.com>
2020-11-30 17:12:35 +0100

    Add some operator examples

--- rxjs/src/app/app.module.ts
+++ rxjs/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HowToPipeComponent } from './how-to-pipe/how-to-pipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HowToPipeComponent
   ],
   imports: [
     BrowserModule
